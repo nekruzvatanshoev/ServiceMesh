329c8854830cadf9b7f9c4370755971b6e5905b4
Nekruz Vatanshoev <nekruz.vatanshoev@outlook.com>
2020-11-11 16:40:09 -0600

    Adding Producer to Spring Boot

---
+++ SpringBoot/src/main/java/com/nekruzvatanshoev/SpringBoot/amqp/Producer.java
@@ -0,0 +1,24 @@
+package com.nekruzvatanshoev.SpringBoot.amqp;
+
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+@Component
+public class Producer {
+    private RabbitTemplate tempalte;
+    private String exchange;
+    private String routingKey;
+
+    Producer(final RabbitTemplate template,
+             @Value("${nekruzvatanshoev.amqp.exchange:}") final String exchange,
+             @Value("${nekruzvatanshoev.amqp.queue}") final String routingKey) {
+        this.tempalte = template;
+        this.exchange = exchange;
+        this.routingKey = routingKey;
+    }
+
+    public void sendMessage(String message) {
+        this.tempalte.convertAndSend(exchange, routingKey, message);
+    }
+}
