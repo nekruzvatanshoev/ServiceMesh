324bd0be4c329de834acc9cd4d759e869ad6fe71
Nekruz Vatanshoev <nekruz.vatanshoev@outlook.com>
2020-11-11 16:43:14 -0600

    Adding a Consumer to Spring Boot

---
+++ SpringBoot/src/main/java/com/nekruzvatanshoev/SpringBoot/amqp/Consumer.java
@@ -0,0 +1,13 @@
+package com.nekruzvatanshoev.SpringBoot.amqp;
+
+import org.springframework.amqp.core.Message;
+import org.springframework.amqp.core.MessageListener;
+import org.springframework.stereotype.Component;
+
+@Component
+public class Consumer implements MessageListener {
+
+    public void onMessage(Message message) {
+        System.out.println(message);
+    }
+}
