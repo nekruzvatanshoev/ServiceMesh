1c2633f1133c52cd2d2e1dfe5973bdaebf1c2e17
Nekruz Vatanshoev <nekruz.vatanshoev@outlook.com>
2020-11-11 19:39:17 -0600

    Added an addToShoppingCart handler within ItemController

--- SpringBoot/src/main/java/com/nekruzvatanshoev/SpringBoot/controller/ItemController.java
+++ SpringBoot/src/main/java/com/nekruzvatanshoev/SpringBoot/controller/ItemController.java
@@ -1,8 +1,24 @@
 package com.nekruzvatanshoev.SpringBoot.controller;
 
+import com.nekruzvatanshoev.SpringBoot.domain.Item;
+import com.nekruzvatanshoev.SpringBoot.service.OrderItemService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class ItemController {
+    public final OrderItemService orderItemService;
 
+    public ItemController(OrderItemService orderItemService) {
+        this.orderItemService = orderItemService;
+    }
+    @PostMapping("/items")
+    public ResponseEntity<Item> addToShoppingCart(@RequestBody Item item) {
+        orderItemService.addItemToCart(item.getId());
+        System.out.println(item);
+        return new ResponseEntity<>(HttpStatus.ACCEPTED);
+    }
 }
--- SpringBoot/src/main/java/com/nekruzvatanshoev/SpringBoot/service/OrderItemService.java
+++ SpringBoot/src/main/java/com/nekruzvatanshoev/SpringBoot/service/OrderItemService.java
@@ -1,8 +1,19 @@
 package com.nekruzvatanshoev.SpringBoot.service;
 
+import com.nekruzvatanshoev.SpringBoot.amqp.Producer;
 import org.springframework.stereotype.Service;
 
 @Service
 public class OrderItemService {
 
+    private final Producer producer;
+
+    public OrderItemService(Producer producer) {
+        this.producer = producer;
+    }
+
+    public String addItemToCart(String id) {
+        producer.sendMessage("Added " + id + " to shopping cart!");
+        return "Added " + id + " shopping to cart!";
+    }
 }
